c60b074583b500e7d06fa18e1c8f6b1a067c3d47
Gabriella Richmond <gabriella.m.richmond@gmail.com>
2024-02-22 07:23:53 -0800

    complete shopping list lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import items from "../data/items";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  
+  const handleClick = () => {
+    setInCart(inCart => !inCart);
+  }
+  
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
+      <li className={inCart? 'in-cart' : ''}>
+        <span>{name}</span>
+        <span className="category">{category}</span>
+        <button className="add" onClick={handleClick}>{inCart ? 'Remove From Cart' : 'Add to Cart'}</button>
+      </li>
   );
 }
 
